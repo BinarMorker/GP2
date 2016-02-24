e6f4c3a23e8eb92d24a683bd92c019662d4d6015
jerped <jerm95@hotmail.com>
2016-02-24 10:41:17 -0500

    ajout du modele de division de depense et des liens entre les modeles

--- modeles/depense.py
+++ modeles/depense.py
@@ -4,7 +4,11 @@ Created on 2016-02-23
 @author: Jeremi Pedneault
 '''
 from sqlalchemy import *
+from sqlalchemy.orm import relationship, backref
+
 from modeles.base import Base
+from modeles.categorie import Categorie
+
 
 class Depense(Base):
     '''
@@ -13,6 +17,14 @@ class Depense(Base):
     
     __tablename__ = "Depenses"
     id = Column(INTEGER, primary_key=True)
+    id_categorie = Column(INTEGER, ForeignKey("Categories.id"))
     nom = Column(String)
     description = Column(String, nullable = True)
     montant = Column(Float)
+    
+    categorie = relationship("Categorie", 
+                           foreign_keys=[id_categorie], 
+                           backref=backref("divisions_depense", 
+                                           uselist=True
+                                           )
+                           )
---
+++ modeles/division_depense.py
@@ -0,0 +1,42 @@
+from sqlalchemy import *
+from sqlalchemy.orm import relationship, backref
+
+from modeles.base import Base
+from modeles.depense import Depense
+
+class Division_Depense(Base):
+    '''
+    classdocs
+    '''
+    
+    __tablename__ = "Divisions_Depenses"
+    id_depense = Column(INTEGER, ForeignKey('Depenses.id'), primary_key=True)
+    id_entite = Column(INTEGER, ForeignKey('Entites.id'), primary_key=True )
+    pourcentage = Column(INTEGER)
+    montant_paye = Column(FLOAT)
+    
+    depense = relationship("Depense", 
+                           foreign_keys=[id_depense], 
+                           backref=backref("divisions_depense", 
+                                           uselist=True
+                                           )
+                           )
+    entite = relationship("Entite", 
+                          foreign_keys=[id_entite], 
+                          backref=backref("divisions_depense", 
+                                          uselist=True
+                                          )
+                          )
+    
+    def montant_total(self):
+        return depense.montant*self.pourcentage/100
+    
+    def montant_a_payer(self):
+        return self.montant_total()-self.montant_paye
+    
+    def est_paye(self):
+        if self.montant_a_payer <= 0:
+            return True
+        else:
+            return False
+            
