8d2fe33077dfedfbc1d8ef9b203215d10bf493e8
obnosim <obnosim@gmail.com>
2016-02-25 15:24:44 -0500

    Connecter les boutons CRUD

--- principale.py
+++ principale.py
@@ -13,6 +13,15 @@ class fenetrePrincipale(QMainWindow):
 		self.ui.boutonDepenses.clicked.connect(self.depenses)
 		self.ui.boutonCategories.clicked.connect(self.categories)
 		self.ui.boutonMembres.clicked.connect(self.membres)
+		self.ui.boutonAjouterDepense.clicked.connect(self.ajouterDepense)
+		self.ui.boutonEditerDepense.clicked.connect(self.modifierDepense)
+		self.ui.boutonRetirerDepense.clicked.connect(self.supprimerDepense)
+		self.ui.boutonAjouterCategorie.clicked.connect(self.ajouterCategorie)
+		self.ui.boutonEditerCategorie.clicked.connect(self.modifierCategorie)
+		self.ui.boutonRetirerCategorie.clicked.connect(self.supprimerCategorie)
+		self.ui.boutonAjouterMembre.clicked.connect(self.ajouterMembre)
+		self.ui.boutonEditerMembre.clicked.connect(self.modifierMembre)
+		self.ui.boutonRetirerMembre.clicked.connect(self.supprimerMembre)
 
 	@pyqtSlot()
 	def derp(self):
@@ -24,9 +33,45 @@ class fenetrePrincipale(QMainWindow):
 		self.ui.onglets.setCurrentWidget(self.ui.ongletDepenses)
 
 	@pyqtSlot()
+	def ajouterDepense(self):
+		print("ajouter dépense")
+
+	@pyqtSlot()
+	def modifierDepense(self):
+		print("modifier dépense")
+
+	@pyqtSlot()
+	def supprimerDepense(self):
+		print("supprimer dépense")
+
+	@pyqtSlot()
 	def categories(self):
 		self.ui.onglets.setCurrentWidget(self.ui.ongletCategories)
 
 	@pyqtSlot()
+	def ajouterCategorie(self):
+		print("ajouter catégorie")
+
+	@pyqtSlot()
+	def modifierCategorie(self):
+		print("ajouter catégorie")
+
+	@pyqtSlot()
+	def supprimerCategorie(self):
+		print("ajouter catégorie")
+
+	@pyqtSlot()
 	def membres(self):
 		self.ui.onglets.setCurrentWidget(self.ui.ongletMembres)
+
+	@pyqtSlot()
+	def ajouterMembre(self):
+		print("ajouter membre")
+
+	@pyqtSlot()
+	def modifierMembre(self):
+		print("ajouter membre")
+
+	@pyqtSlot()
+	def supprimerMembre(self):
+		print("ajouter membre")
