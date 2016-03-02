c4c2c095da783956a0c10f41054de495e53f0687
François Allard <binarmorker@gmail.com>
2016-03-02 09:27:40 -0500

    Base de données en singleton Catégorie et Dépense (controlleurs) mis à jour avec le singleton Retours au lieu de prints dans Catégorie et Dépense (controlleurs)

--- controlleurs/depenses.py
+++ controlleurs/depenses.py
@@ -3,50 +3,34 @@
 '''
 
 from modeles.depense import Depense
-from controlleurs.controlleur import Controlleur
-from sqlalchemy.orm.exc import NoResultFound
 from outils.nombre import Nombre
+from outils.basededonnees import *
 
-class DepensesControlleur(Controlleur):
+class DepensesControlleur:
 	'''
 	Le controlleur des dépenses
 	'''
 
-	def liste_depenses(self):
+	@staticmethod
+	def liste_depenses():
 		'''
 		Montre une liste des dépenses
 		@attention: À venir
 		'''
-		try:
-			depenses = self.session.query(Depense).all()
-			for depense in depenses:
-				print(
-					str(depense.id) + '\n' + 
-					depense.nom + '\n' + 
-					str(depense.montant) + '\n' + 
-					depense.categorie.nom + '\n' + 
-					depense.description + '\n-------')
-				for division in depense.divisions:
-					print(division.entite.nom + ': ' + str(division.pourcentage) + '\n--')
-		except NoResultFound:
-			print('Entrée introuvable')
+		depenses = BaseDeDonnees.Instance().session.query(Depense).all()
+		return depenses
 		
-	def voir_depense(self, depense_id):
+	@staticmethod
+	def voir_depense(depense_id):
 		'''
 		Affiche une dépense
 		@param depense_id: L'identifiant de la dépense à afficher
 		'''
-		try:
-			depense = self.session.query(Depense).filter(Depense.id == depense_id).one()
-			print(str(depense.id) + '\n' + 
-				depense.nom + '\n' + 
-				str(depense.montant) + '\n' + 
-				depense.categorie.nom + '\n' + 
-				depense.description)
-		except NoResultFound:
-			print('Entrée introuvable')
+		depense = BaseDeDonnees.Instance().session.query(Depense).filter(Depense.id == depense_id).one()
+		return depense
 		
-	def ajouter_depense(self, nom, montant, id_categorie, description = ''):
+	@staticmethod
+	def ajouter_depense(nom, montant, id_categorie, description = ''):
 		'''
 		Ajoute une dépense
 		@param nom: Le nom de la nouvelle dépense
@@ -54,27 +38,21 @@ class DepensesControlleur(Controlleur):
 		@param id_categorie: La catégorie de la nouvelle dépenses
 		@param description: La description de la nouvelle dépense
 		'''
-		try:
-			depense = Depense()
-			if nom.strip() == "":
-				raise AssertionError
-			depense.nom = nom
-			if not Nombre.is_float(montant) or float(montant) <= 0:
-				raise AssertionError
-			depense.montant = montant
-			depense.id_categorie = id_categorie
-			depense.description = description
-			self.session.add(depense)
-			self.session.commit()
-			print(str(depense.id) + '\n' + 
-				depense.nom + '\n' + 
-				str(depense.montant) + '\n' + 
-				str(depense.categorie.nom) + '\n' + 
-				depense.description)
-		except AssertionError:
-			print('Le format est invalide')
+		depense = Depense()
+		if nom.strip() == "":
+			raise AssertionError
+		depense.nom = nom
+		if not Nombre.is_float(montant) or float(montant) <= 0:
+			raise AssertionError
+		depense.montant = montant
+		depense.id_categorie = id_categorie
+		depense.description = description
+		BaseDeDonnees.Instance().session.add(depense)
+		BaseDeDonnees.Instance().session.commit()
+		return depense
 		
-	def modifier_depense(self, depense_id, nom = None, montant = None, id_categorie = None, description = None):
+	@staticmethod
+	def modifier_depense(depense_id, nom = None, montant = None, id_categorie = None, description = None):
 		'''
 		Modifie une dépense existante
 		@param depense_id: L'identifiant de la dépense
@@ -83,40 +61,29 @@ class DepensesControlleur(Controlleur):
 		@param id_categorie: La nouvelle catégorie de la dépense
 		@param description: La nouvelle description de la dépense
 		'''
-		try:
-			depense = self.session.query(Depense).filter(Depense.id == depense_id).one()
-			if nom != None:
-				if nom.strip() == "":
-					raise AssertionError
-				depense.nom = nom
-			if montant != None:
-				if not Nombre.is_float(montant) or montant <= 0:
-					raise AssertionError
-				depense.montant = montant
-			if id_categorie != None:
-				depense.id_categorie = id_categorie
-			if description != None:
-				depense.description = description
-			self.session.commit()
-			print(str(depense.id) + '\n' + 
-				depense.nom + '\n' + 
-				str(depense.montant) + '\n' + 
-				str(depense.categorie.nom) + '\n' + 
-				depense.description)
-		except AssertionError:
-			print('Le format est invalide')
-		except NoResultFound:
-			print('Entrée introuvable')
+		depense = BaseDeDonnees.Instance().session.query(Depense).filter(Depense.id == depense_id).one()
+		if nom != None:
+			if nom.strip() == "":
+				raise AssertionError
+			depense.nom = nom
+		if montant != None:
+			if not Nombre.is_float(montant) or montant <= 0:
+				raise AssertionError
+			depense.montant = montant
+		if id_categorie != None:
+			depense.id_categorie = id_categorie
+		if description != None:
+			depense.description = description
+		BaseDeDonnees.Instance().session.commit()
+		return depense
 		
-	def supprimer_depense(self, depense_id):
+	@staticmethod
+	def supprimer_depense(depense_id):
 		'''
 		Supprime une dépense
 		@param depense_id: L'identifiant de la dépense à supprimer
 		'''
-		try:
-			depense = self.session.query(Depense).filter(Depense.id == depense_id).one()
-			self.session.delete(depense)
-			self.session.commit()
-			print(str(depense.id) + '\n' + depense.nom + '\n' + str(depense.montant) + '\n' + depense.description + '\nSupprimé')
-		except NoResultFound:
-			print('Entrée introuvable')
+		depense = BaseDeDonnees.Instance().session.query(Depense).filter(Depense.id == depense_id).one()
+		BaseDeDonnees.Instance().session.delete(depense)
+		BaseDeDonnees.Instance().session.commit()
+		return depense
