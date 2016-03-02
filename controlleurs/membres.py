fa20b97ab13c0ac8a2b94ec53f85232b7f378ad9
Lehoux <6119250@cegepdrummond.ca>
2016-03-02 10:00:23 -0500

    return membre

--- controlleurs/membres.py
+++ controlleurs/membres.py
@@ -3,88 +3,66 @@
 '''
 
 from modeles.membre import Membre
-from controlleurs.controlleur import Controlleur
-from sqlalchemy.orm.exc import NoResultFound
-from outils.nombre import Nombre
-
-
+from outils.basededonnees import *
 
 class MembresControlleur(Controlleur):
 	'''
 	Le controlleur des membre
 	'''
-
-	def liste_membres(self):
+	@staticmethod
+	def liste_membres():
 		'''
 		Montre une liste des membre
 		@attention: À venir
 		'''
-		try:
-			membres = self.session.query(Membre).all()
-			for membre in membres:
-				print(str(membre.id) + '\n' + membre.nom + '\n-------')
-				for distribution_depense in membre.distributions:
-					print(str(distribution_depense.pourcentage) + '\n--')
-		except NoResultFound:
-			print('Entrée introuvable')
-		
-	def voir_membre(self, membre_id):
+		membres = BaseDeDonnees.Instance().session.query(Membre).all()
+		return membres
+	
+	@staticmethod
+	def voir_membre(membre_id):
 		'''
 		Affiche un membre
-		@param entite_id: L'identifiant du membre à afficher
+		@param membre_id: L'identifiant du membre à afficher
 		'''
-		try:
-			membre = self.session.query(Membre).filter(Membre.id == membre_id).one()
-			print(str(membre.id) + '\n' + membre.nom)
-		except NoResultFound:
-			print('Entrée introuvable')
-		
-	def ajouter_membre(self, nom):
+		membre = BaseDeDonnees.Instance().session.query(Membre).filter(Membre.id == membre_id).one()
+		return membre		
+	
+	@staticmethod
+	def ajouter_membre(nom):
 		'''
 		Ajoute un membre
 		@param nom: Le nom du nouveau membre
-		@param montant: Le montant du nouveau membre
-		@param description: La description du nouveau membre
 		'''
-		try:
-			membre = Membre()
-			if nom.strip() == "":
-				raise AssertionError
-			membre.nom = nom
-			self.session.add(membre)
-			self.session.commit()
-			print(str(membre.id) + '\n' + membre.nom)
-		except AssertionError:
-			print('Le format est invalide')
-		
-	def modifier_membre(self, membre_id, nom = None):
+		membre = Membre()
+		if nom.strip() == "":
+			raise AssertionError
+		membre.nom = nom
+		BaseDeDonnees.Instance().session.add(membre)
+		BaseDeDonnees.Instance().session.commit()
+		return membre
+	
+	@staticmethod
+	def modifier_membre(membre_id, nom = None):
 		'''
 		Modifie un membre existant
 		@param membre_id: L'identifiant du membre
 		@param nom: Le nouveau nom du membre
 		'''
-		try:
-			membre = self.session.query(Membre).filter(Membre.id == membre_id).one()
-			if nom != None:
-				if nom.strip() == "":
-					raise AssertionError
-				membre.nom = nom
-			self.session.commit()
-			print(str(membre.id) + '\n' + membre.nom)
-		except AssertionError:
-			print('Le format est invalide')
-		except NoResultFound:
-			print('Entrée introuvable')
-		
-	def supprimer_membre(self, membre_id):
+		membre = BaseDeDonnees.Instance().session.query(Membre).filter(Membre.id == membre_id).one()
+		if nom != None:
+			if nom.strip() == "":
+				raise AssertionError
+			membre.nom = nom
+		BaseDeDonnees.Instance().session.commit()
+		return membre
+	
+	@staticmethod
+	def supprimer_membre(membre_id):
 		'''
 		Supprime une membre
 		@param membre_id: L'identifiant du membre à supprimer
 		'''
-		try:
-			membre = self.session.query(Membre).filter(Membre.id == membre_id).one()
-			self.session.delete(membre)
-			self.session.commit()
-			print(str(membre.id) + '\n' + membre.nom + '\nSupprimé')
-		except NoResultFound:
-			print('Entrée introuvable')
+		membre = BaseDeDonnees.Instance().session.query(Membre).filter(Membre.id == membre_id).one()
+		BaseDeDonnees.Instance().session.delete(membre)
+		BaseDeDonnees.Instance().session.commit()
+		return membre
